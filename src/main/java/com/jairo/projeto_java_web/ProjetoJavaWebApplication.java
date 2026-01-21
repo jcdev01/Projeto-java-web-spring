2b5fd18308deff159259d4cfd1a65b1e60a67513
jairo <jairocostanascimento@gmail.com>
2026-01-21 11:34:53 -0300

    commit inicial projeto java web

---
+++ src/main/java/com/jairo/projeto_java_web/ProjetoJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.jairo.projeto_java_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jairo/projeto_java_web/ProjetoJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jairo.projeto_java_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
