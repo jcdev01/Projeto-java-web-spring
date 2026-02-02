ebceaee044012fb5c0636635adc2c9edd229f9e5
jairo <jairocostanascimento@gmail.com>
2026-02-02 11:48:22 -0300

    criação das classes Payment e associação com as classes Order

--- src/main/java/com/jairo/projeto_java_web/Entites/Order.java
+++ src/main/java/com/jairo/projeto_java_web/Entites/Order.java
@@ -27,6 +27,10 @@ public class Order implements Serializable {
     private User client;
     private Integer orderStatus;
 
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+
+    private Payment payment;
+
 
     @OneToMany(mappedBy = "id.order",fetch = FetchType.EAGER)
     private Set<OrderItem> items=new HashSet<>();
@@ -74,6 +78,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
