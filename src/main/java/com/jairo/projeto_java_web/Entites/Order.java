f6e3998c01ae45946d9a17bd1a754192f51ffa79
jairo <jairocostanascimento@gmail.com>
2026-02-02 12:01:39 -0300

    criação dos metodos getsubtotal e gettotal

--- src/main/java/com/jairo/projeto_java_web/Entites/Order.java
+++ src/main/java/com/jairo/projeto_java_web/Entites/Order.java
@@ -88,6 +88,15 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        Double sum=0.0;
+
+        for (OrderItem x:items){
+        sum+=x.getSubTotal();
+    }
+    return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
